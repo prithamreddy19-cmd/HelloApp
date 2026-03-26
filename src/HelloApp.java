9d1a4c995e67e1c2583dc25c3560cf2298ea20f8
prithamreddy19-cmd <prithamreddy19@gmail.com>
2026-03-27 02:06:44 +0530

    UC6: Combine names using substring method and enhanced for loop

--- src/HelloApp.java
+++ src/HelloApp.java
@@ -4,10 +4,16 @@ public class HelloApp {
         if (args.length == 0) {
             System.out.println("Hello, World!");
         } else {
-            for (String name : args) {
-                System.out.println("Hello, " + name + "!");
-            }
-        }
+            String result = "";
 
+            for (String name : args) {
+                result += name + ", ";
+            }
+
+            // remove last ", "
+            result = result.substring(0, result.length() - 2);
+
+            System.out.println("Hello, " + result + "!");
+        }
     }
 }
